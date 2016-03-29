a00a531c3b484ffd47c7cd67a9454096d47e9baf
Zhihao Yao <yzhh029@gmail.com>
2016-03-28 21:28:23 -0400

    add requestvote rpc

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <thread>
+#include <boost/program_options.hpp>
 
 using namespace std;
 
+
+
 int main() {
+
+    thread t1;
     cout << "Hello, World!" << endl;
     return 0;
 }
---
+++ protocol/Raft.thrift
@@ -0,0 +1,19 @@
+
+namespace cpp raftfs.raft
+
+struct ReqVoteRequest {
+1: required i32 term;
+2: required i32 candidate_id;
+3: optional i64 last_log_index;
+4: optional i32 last_log_term;
+}
+
+struct ReqVoteResponse {
+1: required i32 term;
+2: required bool vote_granted;
+
+}
+
+service RaftService {
+    ReqVoteResponse RequestVote(1:ReqVoteRequest vote);
+}
